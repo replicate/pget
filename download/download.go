f1246799cedeb746f86c4428000ca2c796600a6f
Morgan Fainberg <morgan@replicate.com>
2023-11-12 00:39:57 -0800

    Extract go-routine download bits to func
    extract the goroutine download bits (for a specific chunk) into it's
    own function that is called via the goroutine.
    
    This is useful to isolate for testing.

--- download/download.go
+++ download/download.go
@@ -71,46 +71,7 @@ func FileToBuffer(url string) (*bytes.Buffer, int64, error) {
 		if i == concurrency-1 {
 			end = fileSize - 1
 		}
-
-		go func(start, end int64) {
-			defer wg.Done()
-
-			success := false
-
-			client := newClient()
-
-			req, err := http.NewRequest("GET", trueUrl, nil)
-			if err != nil {
-				// This needs to be a time.Duration to make everything happy
-				fmt.Printf("Error creating request: %v\n", err)
-				errc <- fmt.Errorf("error creating request for %s: %w", req.URL.String(), err)
-			}
-			req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", start, end))
-			resp, err := client.Do(req)
-			if err != nil {
-				fmt.Printf("Error executing request: %v\n", err)
-				errc <- fmt.Errorf("failed to download %s: %w", req.URL.String(), err)
-				return
-			}
-			defer resp.Body.Close()
-
-			n, err := io.ReadFull(resp.Body, data[start:end+1])
-			if err != nil && err != io.EOF {
-				fmt.Printf("Error reading response: %v\n", err)
-				errc <- fmt.Errorf("error reading response for %s: %w", req.URL.String(), err)
-				return
-			}
-			if n != int(end-start+1) {
-				fmt.Printf("Downloaded %d bytes instead of %d\n", n, end-start+1)
-				errc <- fmt.Errorf("downloaded %d bytes instead of %d for %s", n, end-start+1, req.URL.String())
-				return
-			}
-			success = true
-
-			if !success {
-				errc <- fmt.Errorf("failed to download after %d retries", viper.GetInt("retries"))
-			}
-		}(start, end)
+		go downloadChunk(start, end, errc, data, trueUrl, &wg)
 	}
 
 	wg.Wait()
@@ -126,4 +87,44 @@ func FileToBuffer(url string) (*bytes.Buffer, int64, error) {
 
 	buffer := bytes.NewBuffer(data)
 	return buffer, fileSize, nil
+}
+
+func downloadChunk(start, end int64, errc chan<- error, data []byte, trueUrl string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	success := false
+
+	client := newClient()
+
+	req, err := http.NewRequest("GET", trueUrl, nil)
+	if err != nil {
+		// This needs to be a time.Duration to make everything happy
+		fmt.Printf("Error creating request: %v\n", err)
+		errc <- fmt.Errorf("error creating request for %s: %w", req.URL.String(), err)
+	}
+	req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", start, end))
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Error executing request: %v\n", err)
+		errc <- fmt.Errorf("failed to download %s: %w", req.URL.String(), err)
+		return
+	}
+	defer resp.Body.Close()
+
+	n, err := io.ReadFull(resp.Body, data[start:end+1])
+	if err != nil && err != io.EOF {
+		fmt.Printf("Error reading response: %v\n", err)
+		errc <- fmt.Errorf("error reading response for %s: %w", req.URL.String(), err)
+		return
+	}
+	if n != int(end-start+1) {
+		fmt.Printf("Downloaded %d bytes instead of %d\n", n, end-start+1)
+		errc <- fmt.Errorf("downloaded %d bytes instead of %d for %s", n, end-start+1, req.URL.String())
+		return
+	}
+	success = true
+
+	if !success {
+		errc <- fmt.Errorf("failed to download after %d retries", viper.GetInt("retries"))
+	}
 }

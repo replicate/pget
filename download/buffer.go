660be2dceaa5260f4bf7ce39df6b17f1b79b33ea
Morgan Fainberg <morgan@replicate.com>
2023-11-13 13:05:10 -0800

    Use ErrGroup not WaitGroup
    Since we're already doing errorhandling, lets streamline this with
    the use of an ErrGroup.
    
    In implementing use, the downloadChunk was extracted to make the
    code easier to follow.

--- download/buffer.go
+++ download/buffer.go
@@ -2,15 +2,16 @@ package download
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"io"
 	"math"
 	"net/http"
-	"sync"
 	"time"
 
 	"github.com/dustin/go-humanize"
 	"github.com/spf13/viper"
+	"golang.org/x/sync/errgroup"
 
 	"github.com/replicate/pget/optname"
 )
@@ -39,10 +40,10 @@ func getRemoteFileSize(url string) (string, int64, error) {
 	return trueUrl, fileSize, nil
 }
 
-func DownloadFileToBuffer(url string) (*bytes.Buffer, int64, error) {
+func FileToBuffer(url string) (*bytes.Buffer, int64, error) {
 	maxConcurrency := viper.GetInt(optname.Concurrency)
 
-	trueUrl, fileSize, err := getRemoteFileSize(url)
+	trueURL, fileSize, err := getRemoteFileSize(url)
 	if err != nil {
 		return nil, -1, err
 	}
@@ -66,11 +67,9 @@ func DownloadFileToBuffer(url string) (*bytes.Buffer, int64, error) {
 		fmt.Printf("Downloading %s bytes with %d connections (chunk-size = %s)\n", humanize.Bytes(uint64(fileSize)), concurrency, humanize.Bytes(uint64(chunkSize)))
 	}
 
-	var wg sync.WaitGroup
-	wg.Add(concurrency)
+	errGroup, ctx := errgroup.WithContext(context.Background())
 
 	data := make([]byte, fileSize)
-	errc := make(chan error, concurrency)
 	startTime := time.Now()
 
 	for i := 0; i < concurrency; i++ {
@@ -81,48 +80,43 @@ func DownloadFileToBuffer(url string) (*bytes.Buffer, int64, error) {
 			end = fileSize - 1
 		}
 
-		go func(start, end int64) {
-			defer wg.Done()
-			client := newClient()
-
-			req, err := http.NewRequest("GET", trueUrl, nil)
-			if err != nil {
-				fmt.Printf("Error creating request: %v\n", err)
-				errc <- fmt.Errorf("failed to download %s", req.URL.String())
-				return
-			}
-			req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", start, end))
-			resp, err := client.Do(req)
-			if err != nil {
-				fmt.Printf("Error executing request: %v\n", err)
-				errc <- fmt.Errorf("error executing request for %s: %w", req.URL.String(), err)
-				return
-			}
-			defer resp.Body.Close()
-
-			n, err := io.ReadFull(resp.Body, data[start:end+1])
-			if err != nil && err != io.EOF {
-				fmt.Printf("Error reading response: %v\n", err)
-				errc <- fmt.Errorf("error reading response for %s: %w", req.URL.String(), err)
-			}
-			if n != int(end-start+1) {
-				fmt.Printf("Downloaded %d bytes instead of %d\n", n, end-start+1)
-				errc <- fmt.Errorf("downloaded %d bytes instead of %d for %s", n, end-start+1, req.URL.String())
-			}
-		}(start, end)
+		errGroup.Go(func() error {
+			return downloadChunk(ctx, start, end, data[start:end+1], trueURL)
+		})
 	}
 
-	wg.Wait()
-	close(errc) // close the error channel
-	for err := range errc {
-		if err != nil {
-			return nil, -1, err // return the first error we encounter
-		}
+	if err := errGroup.Wait(); err != nil {
+		return nil, -1, err // return the first error we encounter
 	}
+
 	elapsed := time.Since(startTime).Seconds()
 	througput := humanize.Bytes(uint64(float64(fileSize) / elapsed))
 	fmt.Printf("Downloaded %s bytes in %.3fs (%s/s)\n", humanize.Bytes(uint64(fileSize)), elapsed, througput)
 
 	buffer := bytes.NewBuffer(data)
 	return buffer, fileSize, nil
+}
+
+func downloadChunk(ctx context.Context, start, end int64, dataSlice []byte, trueURL string) error {
+	client := newClient()
+	req, err := http.NewRequestWithContext(ctx, "GET", trueURL, nil)
+	if err != nil {
+		return fmt.Errorf("failed to download %s", req.URL.String())
+	}
+	req.Header.Set("Range", fmt.Sprintf("bytes=%d-%d", start, end))
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("Error executing request: %v\n", err)
+		return fmt.Errorf("error executing request for %s: %w", req.URL.String(), err)
+	}
+	defer resp.Body.Close()
+
+	n, err := io.ReadFull(resp.Body, dataSlice)
+	if err != nil && err != io.EOF {
+		return fmt.Errorf("error reading response for %s: %w", req.URL.String(), err)
+	}
+	if n != int(end-start+1) {
+		return fmt.Errorf("downloaded %d bytes instead of %d for %s", n, end-start+1, req.URL.String())
+	}
+	return nil
 }

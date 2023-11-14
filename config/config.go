7f878eac16b7bcad452489634d73737407d75ce1
Morgan Fainberg <morgan@replicate.com>
2023-11-14 10:34:45 -0800

    Add --resolve argument
    Add the --resolve argument. May be specified multiple times. Format
    <hostname>:<port>:<IPAddr>, this flag overrides name resolution for
    a given hostname. This is useful for debugging, testing, or any case
    where it is desireable to avoid the DNS lookup but still work within
    the standard context of SSL, Hosts, etc in HTTP

--- config/config.go
+++ config/config.go
@@ -1,9 +1,11 @@
 package config
 
 import (
+	"fmt"
 	"github.com/replicate/pget/optname"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"net"
 	"runtime"
 	"strings"
 	"time"
@@ -15,9 +17,13 @@ var (
 	Extract          bool
 	Force            bool
 	MinimumChunkSize string
+	ResolveHosts     []string
 	Retries          int
 	Verbose          bool
 )
+
+// HostToIPResolutionMap is a map of hostnames to IP addresses
+var HostToIPResolutionMap = make(map[string]string)
 
 func AddFlags(cmd *cobra.Command) {
 	cmd.PersistentFlags().IntVarP(&Concurrency, optname.Concurrency, "c", runtime.GOMAXPROCS(0)*4, "Maximum number of concurrent downloads")
@@ -25,6 +31,7 @@ func AddFlags(cmd *cobra.Command) {
 	cmd.PersistentFlags().BoolVarP(&Extract, optname.Extract, "x", false, "Extract archive after download")
 	cmd.PersistentFlags().StringVarP(&MinimumChunkSize, optname.MinimumChunkSize, "m", "16M", "Minimum chunk size (in bytes) to use when downloading a file (e.g. 10M)")
 	cmd.PersistentFlags().BoolVarP(&Force, optname.Force, "f", false, "Force download, overwriting existing file")
+	cmd.PersistentFlags().StringSliceVar(&ResolveHosts, optname.Resolve, []string{}, "Resolve hostnames to specific IPs")
 	cmd.PersistentFlags().IntVarP(&Retries, optname.Retries, "r", 5, "Number of retries when attempting to retrieve a file")
 	cmd.PersistentFlags().BoolVarP(&Verbose, optname.Verbose, "v", false, "Verbose mode")
 
@@ -34,4 +41,38 @@ func AddFlags(cmd *cobra.Command) {
 	if err := viper.BindPFlags(cmd.PersistentFlags()); err != nil {
 		panic(err)
 	}
+}
+
+func PersistentStartupProcessFlags() error {
+
+	if err := convertResolveHostsToMap(); err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func convertResolveHostsToMap() error {
+	for _, resolveHost := range viper.GetStringSlice(optname.Resolve) {
+		split := strings.SplitN(resolveHost, ":", 3)
+		if len(split) != 3 {
+			return fmt.Errorf("invalid resolve host format, expected <hostname>:port:<ip>, got: %s", resolveHost)
+		}
+		host, port, addr := strings.ToLower(split[0]), split[1], split[2]
+		hostPort := fmt.Sprintf("%s:%s", host, port)
+		if _, ok := HostToIPResolutionMap[hostPort]; ok {
+			return fmt.Errorf("duplicate host specified: %s", host)
+		}
+		IP := net.ParseIP(addr)
+		if IP == nil {
+			return fmt.Errorf("invalid IP address: %s", addr)
+		}
+		HostToIPResolutionMap[hostPort] = fmt.Sprintf("%s:%s", IP.String(), port)
+	}
+	if viper.GetBool(optname.Verbose) {
+		for key, elem := range HostToIPResolutionMap {
+			fmt.Printf("CONFIG: Resolving %s to %s\n", key, elem)
+		}
+	}
+	return nil
 }

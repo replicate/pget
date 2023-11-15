06304f9ab91ef88b4441abf7719853a1b55f7e29
Morgan Fainberg <morgan@replicate.com>
2023-11-15 10:57:25 -0800

    Add Linter support

---
+++ tools.go
@@ -0,0 +1,11 @@
+//go:build tools
+// +build tools
+
+// https://github.com/go-modules-by-example/index/blob/master/010_tools/README.md
+
+package tools
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "golang.org/x/tools/cmd/goimports"
+)

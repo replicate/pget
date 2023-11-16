282dcb98460b8b6bd72366106a9cf1d24d304bff
Morgan Fainberg <morgan@replicate.com>
2023-11-16 12:36:20 -0800

    Do not run AddFlags twice

--- main.go
+++ main.go
@@ -5,11 +5,9 @@ import (
 	"os"
 
 	"github.com/replicate/pget/cmd"
-	"github.com/replicate/pget/pkg/config"
 )
 
 func main() {
-	config.AddFlags(cmd.RootCMD)
 	if err := cmd.RootCMD.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)

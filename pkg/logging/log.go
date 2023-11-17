02f7a9fac21d6358d36a99a872c84f0dd828d20c
Morgan Fainberg <morgan@replicate.com>
2023-11-17 10:41:31 -0800

    Disable console colorization of logs
    TODO: make this configurable if anyone cares

--- pkg/logging/log.go
+++ pkg/logging/log.go
@@ -13,7 +13,8 @@ import (
 var Logger zerolog.Logger
 
 func init() {
-	output := zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339}
+	// TODO: Make color configurable? Disabled so we don't have to deal with ANSI escape codes in our logoutput
+	output := zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339, NoColor: true}
 	output.FormatLevel = func(i interface{}) string {
 		return strings.ToUpper(fmt.Sprintf("| %-6s|", i))
 	}

3fc25c1d799a00080f16e51f10d5ba97707d36fc
Philip Potter <phil@replicate.com>
2023-11-23 13:54:08 +0000

    Fix test flake
    TestDownloadFilesFromHost was flaking.  This was because we were writing to
    dummyMode.args and dummyMode.timesCalled in an unsynchronised way from two
    different goroutines.  This meant that we could get lost updates.
    
    I fixed this by adding a channel.  dummyMode.DownloadFile now sends its args to
    a channel; once you're done with your test input, you call dummyMode.Args() to
    read everything off the channel and put it into the slice as before.

--- cmd/multifile/multifile_test.go
+++ cmd/multifile/multifile_test.go
@@ -20,25 +20,60 @@ type dummyModeCallerArgs struct {
 }
 
 type dummyMode struct {
-	timesCalled int
-	args        []dummyModeCallerArgs
-	returnErr   bool
+	args      []dummyModeCallerArgs
+	returnErr bool
+	calls     chan dummyModeCallerArgs
 }
 
 // ensure *dummyMode implements download.Mode
 var _ download.Mode = &dummyMode{}
 
 func (d *dummyMode) DownloadFile(url string, dest string) (int64, time.Duration, error) {
-	d.timesCalled++
-	d.args = append(d.args, dummyModeCallerArgs{url, dest})
+	d.calls <- dummyModeCallerArgs{url, dest}
 	if d.returnErr {
 		return -1, time.Duration(0), errors.New("test error")
 	}
 	return 100, time.Duration(1) * time.Second, nil
 }
 
+// Args returns the args that DownloadFile was called with.
+func (d *dummyMode) Args() []dummyModeCallerArgs {
+DONE:
+	// non-blocking read the whole channel into d.args
+	for {
+		select {
+		case args := <-d.calls:
+			d.args = append(d.args, args)
+		default:
+			break DONE
+		}
+	}
+	return d.args
+}
+
+func (d *dummyMode) Arg(i int) dummyModeCallerArgs {
+	return d.Args()[i]
+}
+
+// initializeDummyMode returns a download.Mode that returns an error if returnErr is true
+// This function returns a download.Mode instead of a *dummyMode so that we can ensure
+// that *dummyMode implements download.Mode
+func initializeDummyMode(returnErr bool) download.Mode {
+	dummy := &dummyMode{
+		returnErr: returnErr,
+		calls:     make(chan dummyModeCallerArgs, 100),
+	}
+	return dummy
+}
+
+// getDummyMode returns a dummyMode wrapping initializeDummyMode
+// tests should use getDummyMode instead of initializeDummyMode
+func getDummyMode(returnErr bool) *dummyMode {
+	return initializeDummyMode(returnErr).(*dummyMode)
+}
+
 func TestDownloadFilesFromHost(t *testing.T) {
-	mode := &dummyMode{returnErr: false}
+	mode := getDummyMode(false)
 
 	entries := []manifestEntry{
 		{"https://example.com/file1.txt", "/tmp/file1.txt"},
@@ -53,22 +88,22 @@ func TestDownloadFilesFromHost(t *testing.T) {
 	_ = downloadFilesFromHost(mode, eg, entries, metrics)
 	err := eg.Wait()
 	assert.NoError(t, err)
-	assert.Equal(t, 2, mode.timesCalled)
-	assert.Contains(t, mode.args, dummyModeCallerArgs{entries[0].url, entries[0].dest})
-	assert.Contains(t, mode.args, dummyModeCallerArgs{entries[1].url, entries[1].dest})
+	assert.Equal(t, 2, len(mode.Args()))
+	assert.Contains(t, mode.Args(), dummyModeCallerArgs{entries[0].url, entries[0].dest})
+	assert.Contains(t, mode.Args(), dummyModeCallerArgs{entries[1].url, entries[1].dest})
 
-	failsMode := &dummyMode{returnErr: true}
+	failsMode := getDummyMode(true)
 
 	eg = initializeErrGroup()
 	_ = downloadFilesFromHost(failsMode, eg, entries, metrics)
 	err = eg.Wait()
+	_ = failsMode.Args()
 	assert.Error(t, err)
-
 }
 
 func TestDownloadAndMeasure(t *testing.T) {
 
-	mode := &dummyMode{returnErr: false}
+	mode := getDummyMode(false)
 
 	metrics := &downhloadMetrics{
 		mut: sync.Mutex{},
@@ -78,9 +113,9 @@ func TestDownloadAndMeasure(t *testing.T) {
 	dest := "/tmp/file1.txt"
 	err := downloadAndMeasure(mode, url, dest, metrics)
 	assert.NoError(t, err)
-	assert.Equal(t, 1, mode.timesCalled)
-	assert.Equal(t, url, mode.args[0].url)
-	assert.Equal(t, dest, mode.args[0].dest)
+	assert.Equal(t, 1, len(mode.Args()))
+	assert.Equal(t, url, mode.Arg(0).url)
+	assert.Equal(t, dest, mode.Arg(0).dest)
 }
 
 func TestAddDownloadMetrics(t *testing.T) {

f87c90ad08add5a1738f37304a43753b556ef760
Morgan Fainberg <morgan@replicate.com>
2023-11-22 16:05:52 -0800

    Implement tests for multifile logic
    Independent of the manifest processing logic, implement testing
    for the multifile logic.

--- pkg/download/modes.go
+++ pkg/download/modes.go
@@ -1,6 +1,9 @@
 package download
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type modeFactoryFunc func() Mode
 
@@ -15,4 +18,14 @@ type Mode interface {
 
 func GetMode(name string) Mode {
 	return modes[name]()
+}
+
+// AddMode registers a new mode with the given name, this is intended for use with testing only
+// make sure to call the returned function to clean up after the test is done
+func AddMode(name string, factory modeFactoryFunc) (func(), error) {
+	if _, exists := modes[name]; exists {
+		return nil, fmt.Errorf("mode %s already exists", name)
+	}
+	modes[name] = factory
+	return func() { delete(modes, name) }, nil
 }

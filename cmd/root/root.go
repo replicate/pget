replicate/pget

commit 190138ee9af93b8b96fd67750f0c51bd470e87d0

Remove viper calls from client and download packages
It was hard to test and maintain this code because it was making calls out to
get configuration from global state.

This limits handling of configuration to cmd, pkg/cli, and pkg/config.

I removed the `ignore-retry-after` option because I couldn't find any usage of
it and YAGNI.

cmd/root/root.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/dustin/go-humanize"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -104,17 +105,23 @@ func rootExecute(urlString, dest string) error {
 	_ = os.WriteFile(tmpFile, []byte(""), 0644)
 	defer os.Remove(tmpFile)
 
-	modeName := download.BufferModeName
-	if viper.GetBool(optname.Extract) {
-		modeName = download.ExtractTarModeName
+	minChunkSize, err := humanize.ParseBytes(viper.GetString(optname.MinimumChunkSize))
+	if err != nil {
+		return err
 	}
-	clientOpts := client.HTTPClientOpts{
+
+	clientOpts := client.Options{
 		MaxConnPerHost: viper.GetInt(optname.MaxConnPerHost),
 		ForceHTTP2:     viper.GetBool(optname.ForceHTTP2),
 		MaxRetries:     viper.GetInt(optname.Retries),
+		ConnectTimeout: viper.GetDuration(optname.ConnTimeout),
 	}
-	httpClient := client.NewHTTPClient(clientOpts)
-	mode, err := download.GetMode(modeName, httpClient)
+	downloadOpts := download.Options{
+		MaxChunks:    viper.GetInt(optname.MaxChunks),
+		MinChunkSize: int64(minChunkSize),
+		Client:       clientOpts,
+	}
+	mode, err := download.GetMode(download.BufferModeName, downloadOpts)
 	if err != nil {
 		return fmt.Errorf("error getting mode: %w", err)
 	}

cmd/multifile/multifile.go
@@ -119,13 +119,23 @@ func initializeErrGroup() *errgroup.Group {
 func multifileExecute(manifest manifest) error {
 	logger := logging.GetLogger()
 
-	clientOpts := client.HTTPClientOpts{
+	minChunkSize, err := humanize.ParseBytes(viper.GetString(optname.MinimumChunkSize))
+	if err != nil {
+		return err
+	}
+
+	clientOpts := client.Options{
 		MaxConnPerHost: viper.GetInt(optname.MaxConnPerHost),
 		ForceHTTP2:     viper.GetBool(optname.ForceHTTP2),
 		MaxRetries:     viper.GetInt(optname.Retries),
+		ConnectTimeout: viper.GetDuration(optname.ConnTimeout),
 	}
-	httpClient := client.NewHTTPClient(clientOpts)
-	mode, err := download.GetMode(download.BufferModeName, httpClient)
+	downloadOpts := download.Options{
+		MaxChunks:    viper.GetInt(optname.MaxChunks),
+		MinChunkSize: int64(minChunkSize),
+		Client:       clientOpts,
+	}
+	mode, err := download.GetMode(download.BufferModeName, downloadOpts)
 	if err != nil {
 		return fmt.Errorf("error getting mode: %w", err)
 	}

pkg/client/client.go
@@ -10,11 +10,9 @@ import (
 	"time"
 
 	"github.com/hashicorp/go-retryablehttp"
-	"github.com/spf13/viper"
 
 	"github.com/replicate/pget/pkg/config"
 	"github.com/replicate/pget/pkg/logging"
-	"github.com/replicate/pget/pkg/optname"
 	"github.com/replicate/pget/pkg/version"
 )
 
@@ -36,21 +34,22 @@ func (c *HTTPClient) Do(req *http.Request) (*http.Response, error) {
 	return c.Client.Do(req)
 }
 
-type HTTPClientOpts struct {
+type Options struct {
 	ForceHTTP2     bool
 	MaxConnPerHost int
 	MaxRetries     int
+	ConnectTimeout time.Duration
 }
 
 // NewHTTPClient factory function returns a new http.Client with the appropriate settings and can limit number of clients
 // per host if the MaxConnPerHost option is set.
-func NewHTTPClient(opts HTTPClientOpts) *HTTPClient {
+func NewHTTPClient(opts Options) *HTTPClient {
 	disableKeepAlives := !opts.ForceHTTP2
 
 	transport := &http.Transport{
 		Proxy: http.ProxyFromEnvironment,
 		DialContext: transportDialContext(&net.Dialer{
-			Timeout:   viper.GetDuration(optname.ConnTimeout),
+			Timeout:   opts.ConnectTimeout,
 			KeepAlive: 30 * time.Second,
 		}),
 		ForceAttemptHTTP2:     opts.ForceHTTP2,
@@ -112,7 +111,7 @@ func evaluateRetryAfter(resp *http.Response) time.Duration {
 }
 
 func shouldApplyRetryAfter(resp *http.Response) bool {
-	return !viper.GetBool(optname.IgnoreRetryAfter) && resp != nil && resp.StatusCode == http.StatusTooManyRequests
+	return resp != nil && resp.StatusCode == http.StatusTooManyRequests
 }
 
 // checkRedirectFunc is a wrapper around http.Client.CheckRedirect that allows for printing out redirects

pkg/config/config.go
@@ -43,7 +43,6 @@ func AddRootPersistentFlags(cmd *cobra.Command) error {
 	cmd.PersistentFlags().IntVarP(&Retries, optname.Retries, "r", 5, "Number of retries when attempting to retrieve a file")
 	cmd.PersistentFlags().BoolVarP(&Verbose, optname.Verbose, "v", false, "Verbose mode (equivalent to --log-level debug)")
 	cmd.PersistentFlags().StringVar(&LoggingLevel, optname.LoggingLevel, "info", "Log level (debug, info, warn, error)")
-	cmd.PersistentFlags().BoolVar(&IgnoreRetryAfter, optname.IgnoreRetryAfter, false, "Ignore Retry-After header")
 	cmd.PersistentFlags().BoolVar(&ForceHTTP2, optname.ForceHTTP2, false, "Force HTTP/2")
 
 	viper.SetEnvPrefix("PGET")

pkg/download/buffer.go
@@ -9,21 +9,21 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"runtime"
 	"time"
 
 	"github.com/dustin/go-humanize"
-	"github.com/spf13/viper"
 	"golang.org/x/sync/errgroup"
 
 	"github.com/replicate/pget/pkg/client"
 	"github.com/replicate/pget/pkg/logging"
-	"github.com/replicate/pget/pkg/optname"
 )
 
 const BufferModeName = "buffer"
 
 type BufferMode struct {
 	Client *client.HTTPClient
+	Options
 }
 
 type Target struct {
@@ -37,10 +37,28 @@ func (t *Target) Basename() string {
 	return filepath.Base(t.Dest)
 }
 
-func getBufferMode(client *client.HTTPClient) Mode {
+func getBufferMode(opts Options) Mode {
+	client := client.NewHTTPClient(opts.Client)
 	return &BufferMode{
-		Client: client,
+		Client:  client,
+		Options: opts,
 	}
+}
+
+func (m *BufferMode) maxChunks() int {
+	maxChunks := m.MaxChunks
+	if maxChunks == 0 {
+		return runtime.GOMAXPROCS(0) * 4
+	}
+	return maxChunks
+}
+
+func (m *BufferMode) minChunkSize() int64 {
+	minChunkSize := m.MinChunkSize
+	if minChunkSize == 0 {
+		return 16 * 1024 * 1024
+	}
+	return minChunkSize
 }
 
 func (m *BufferMode) getRemoteFileSize(ctx context.Context, target Target) (string, int64, error) {
@@ -70,7 +88,6 @@ func (m *BufferMode) getRemoteFileSize(ctx context.Context, target Target) (stri
 }
 
 func (m *BufferMode) fileToBuffer(ctx context.Context, target Target) (*bytes.Buffer, int64, error) {
-	maxChunks := viper.GetInt(optname.MaxChunks)
 	logger := logging.GetLogger()
 
 	trueURL, fileSize, err := m.getRemoteFileSize(ctx, target)
@@ -81,14 +98,9 @@ func (m *BufferMode) fileToBuffer(ctx context.Context, target Target) (*bytes.Bu
 		target.TrueURL = trueURL
 	}
 
-	// TODO split this into separate functions
-	minChunkSize, err := humanize.ParseBytes(viper.GetString(optname.MinimumChunkSize))
-	if err != nil {
-		return nil, -1, fmt.Errorf("unable to parse minimum chunk size: %v", err)
-	}
-	chunkSize := fileSize / int64(maxChunks)
-	if chunkSize < int64(minChunkSize) {
-		chunkSize = int64(minChunkSize)
+	chunkSize := fileSize / int64(m.maxChunks())
+	if chunkSize < m.minChunkSize() {
+		chunkSize = m.minChunkSize()
 	}
 	if chunkSize < 0 {
 		return nil, -1, fmt.Errorf("error: chunksize incorrect - result is negative, %d", chunkSize)
@@ -96,8 +108,8 @@ func (m *BufferMode) fileToBuffer(ctx context.Context, target Target) (*bytes.Bu
 	// not more than one connection per min chunk size
 	chunks := int(math.Ceil(float64(fileSize) / float64(chunkSize)))
 
-	if chunks > maxChunks {
-		chunks = maxChunks
+	if chunks > m.maxChunks() {
+		chunks = m.maxChunks()
 		chunkSize = fileSize / int64(chunks)
 	}
 	logger.Debug().Str("dest", target.Dest).

pkg/download/modes.go
@@ -7,10 +7,22 @@ import (
 	"github.com/replicate/pget/pkg/client"
 )
 
-type modeFactory func(client *client.HTTPClient) Mode
+type modeFactory func(opts Options) Mode
 
 type Mode interface {
 	DownloadFile(url string, dest string) (fileSize int64, elapsedTime time.Duration, err error)
+}
+
+type Options struct {
+
+	// Maximum number of chunks to download. If set to zero, GOMAXPROCS*4
+	// will be used.
+	MaxChunks int
+
+	// Minimum number of bytes per chunk. If set to zero, 16 MiB will be
+	// used.
+	MinChunkSize int64
+	Client       client.Options
 }
 
 func modeFactories() map[string]modeFactory {
@@ -20,10 +32,10 @@ func modeFactories() map[string]modeFactory {
 	}
 }
 
-func GetMode(name string, client *client.HTTPClient) (Mode, error) {
+func GetMode(name string, opts Options) (Mode, error) {
 	factory, ok := modeFactories()[name]
 	if !ok {
 		return nil, fmt.Errorf("unknown mode: %s", name)
 	}
-	return factory(client), nil
+	return factory(opts), nil
 }

pkg/download/modes_test.go
@@ -1,49 +1,0 @@
-package download
-
-import (
-	"testing"
-
-	"github.com/replicate/pget/pkg/client"
-
-	"github.com/spf13/viper"
-	"github.com/stretchr/testify/assert"
-
-	"github.com/replicate/pget/pkg/optname"
-)
-
-func TestGetMode(t *testing.T) {
-	clientOpts := client.HTTPClientOpts{
-		MaxConnPerHost: viper.GetInt(optname.MaxConnPerHost),
-		ForceHTTP2:     viper.GetBool(optname.ForceHTTP2),
-		MaxRetries:     viper.GetInt(optname.Retries),
-	}
-	httpClient := client.NewHTTPClient(clientOpts)
-	testCases := []struct {
-		name     string
-		modeName string
-		mode     Mode
-		err      bool
-	}{
-		{"Get BufferMode", BufferModeName, &BufferMode{}, false},
-		{"Get ExtractTarMode", ExtractTarModeName, &ExtractTarMode{}, false},
-		{"Get Unknown Mode", "invalid", nil, true},
-	}
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			mode, err := GetMode(tc.modeName, httpClient)
-			assert.IsType(t, tc.mode, mode)
-			assert.Equal(t, tc.err, err != nil)
-		})
-	}
-}
-
-func TestGetBufferMode(t *testing.T) {
-	clientOpts := client.HTTPClientOpts{
-		MaxConnPerHost: viper.GetInt(optname.MaxConnPerHost),
-		ForceHTTP2:     viper.GetBool(optname.ForceHTTP2),
-		MaxRetries:     viper.GetInt(optname.Retries),
-	}
-	httpClient := client.NewHTTPClient(clientOpts)
-
-	assert.IsType(t, &BufferMode{}, getBufferMode(httpClient))
-}

pkg/download/tar_extraction.go
@@ -15,10 +15,12 @@ type ExtractTarMode struct {
 	BufferMode
 }
 
-func getExtractTarMode(client *client.HTTPClient) Mode {
+func getExtractTarMode(opts Options) Mode {
+	client := client.NewHTTPClient(opts.Client)
 	return &ExtractTarMode{
 		BufferMode: BufferMode{
-			Client: client,
+			Client:  client,
+			Options: opts,
 		},
 	}
 }

commit 597cfed6c2e34e1b4eac369542d1760a34c0977c

Fix extract mode
I broke this in 190138e, whoops

cmd/root/root.go
@@ -106,7 +106,11 @@ func rootExecute(ctx context.Context, urlString, dest string) error {
 		MinChunkSize: int64(minChunkSize),
 		Client:       clientOpts,
 	}
-	mode, err := download.GetMode(download.BufferModeName, downloadOpts)
+	modeName := download.BufferModeName
+	if viper.GetBool(optname.Extract) {
+		modeName = download.ExtractTarModeName
+	}
+	mode, err := download.GetMode(modeName, downloadOpts)
 	if err != nil {
 		return fmt.Errorf("error getting mode: %w", err)
 	}
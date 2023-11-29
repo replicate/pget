0375abed8bfa2accf557d096c68d638c38ff9c30
Morgan Fainberg <morgan@replicate.com>
2023-11-29 15:08:47 -0800

    Minor stylistic fixes
    * Do not prefix the function name with the same name as the package
    * Lower case for err messages is preferred
    * Remove unused constant
    * Extracter -> Extractor (fix spelling)

---
+++ pkg/consumer/tar_extractor.go
@@ -0,0 +1,20 @@
+package consumer
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/replicate/pget/pkg/extract"
+)
+
+type TarExtractor struct{}
+
+var _ Consumer = &TarExtractor{}
+
+func (f *TarExtractor) Consume(reader io.Reader, destPath string) error {
+	err := extract.TarFile(reader, destPath)
+	if err != nil {
+		return fmt.Errorf("error extracting file: %w", err)
+	}
+	return nil
+}

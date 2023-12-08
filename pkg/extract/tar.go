317a2ccb7620215465a93249cd16b895a078c7d8
Morgan Fainberg <morgan@replicate.com>
2023-12-08 13:43:36 -0800

    BUGFIX: Fix tar extraction
    Fix tar extraction:
    * Make -x function again for rootCMD
    * Properly handle hard-links in tar files.
      * implement testing for the tar extraction code
    
    Closes: #87
    Closes: #86

--- pkg/extract/tar.go
+++ pkg/extract/tar.go
@@ -11,7 +11,15 @@ import (
 	"github.com/replicate/pget/pkg/logging"
 )
 
+type link struct {
+	linkType byte
+	oldName  string
+	newName  string
+}
+
 func TarFile(reader io.Reader, destDir string) error {
+	var links []*link
+
 	startTime := time.Now()
 	tarReader := tar.NewReader(reader)
 	logger := logging.GetLogger()
@@ -49,20 +57,49 @@ func TarFile(reader io.Reader, destDir string) error {
 				targetFile.Close()
 				return err
 			}
-			targetFile.Close()
-		case tar.TypeSymlink:
-			if err := os.Symlink(header.Linkname, target); err != nil {
-				return err
+			if err := targetFile.Close(); err != nil {
+				return fmt.Errorf("error closing file %s: %w", target, err)
 			}
+		case tar.TypeSymlink, tar.TypeLink:
+			// Defer creation of
+			links = append(links, &link{linkType: header.Typeflag, oldName: header.Linkname, newName: target})
 		default:
 			return fmt.Errorf("unsupported file type for %s, typeflag %s", header.Name, string(header.Typeflag))
 		}
 	}
+
+	if err := createLinks(links, destDir); err != nil {
+		return fmt.Errorf("error creating links: %w", err)
+	}
+
 	elapsed := time.Since(startTime).Seconds()
 	logger.Debug().
 		Str("extractor", "tar").
 		Float64("elapsed_time", elapsed).
 		Str("status", "complete").
 		Msg("Extract")
+	return nil
+}
+
+func createLinks(links []*link, destDir string) error {
+	for _, link := range links {
+		targetDir := filepath.Dir(link.newName)
+		if err := os.MkdirAll(targetDir, 0755); err != nil {
+			return err
+		}
+		switch link.linkType {
+		case tar.TypeLink:
+			oldPath := filepath.Join(destDir, link.oldName)
+			if err := os.Link(oldPath, link.newName); err != nil {
+				return fmt.Errorf("error creating hard link from %s to %s: %w", oldPath, link.newName, err)
+			}
+		case tar.TypeSymlink:
+			if err := os.Symlink(link.oldName, link.newName); err != nil {
+				return fmt.Errorf("error creating symlink from %s to %s: %w", link.oldName, link.newName, err)
+			}
+		default:
+			return fmt.Errorf("unsupported link type %s", string(link.linkType))
+		}
+	}
 	return nil
 }

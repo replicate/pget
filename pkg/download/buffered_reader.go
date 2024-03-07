42042a50622ae83f3e1daf486b210f08a97fecd4
Philip Potter <phil@replicate.com>
2024-03-07 12:23:37 +0000

    reimplement bufferedReader on top of bufio.Reader
    This reimplements bufferedReader on top of bufio.Reader.
    
    This doens't make the code shorter, but it delegates more complexity to standard
    library functions.  In particular, bufio.Reader:
    
    - handles storing errors and passing them out via Read() where appropriate
    - handles errors where a read would exceed the buffer size
    
    Furthermore this commit removes any HTTP concerns from bufferedReader.  The
    downloadBody() function has been changed to a generic ReadFrom().

--- pkg/download/buffered_reader.go
+++ pkg/download/buffered_reader.go
@@ -1,26 +1,30 @@
 package download
 
 import (
-	"bytes"
-	"fmt"
+	"bufio"
 	"io"
-	"net/http"
+	"strings"
 	"sync"
 )
 
-// A bufferedReader wraps an http.Response.Body so that it can be eagerly
-// downloaded to a buffer before the actual io.Reader consumer can read it.
-// It implements io.Reader.
+// A bufferedReader wraps a bufio.Reader so that it can be shared between
+// goroutines, with one fetching data from an upstream reader and another
+// reading the data.  It implements io.ReaderFrom and io.Reader.  Read() will
+// block until Done() is called.
+//
+// The intended use is: one goroutine calls Read(), which blocks until data is
+// ready.  Another calls ReadFrom() and then Done().  The call to Done()
+// unblocks the Read() call and allows it to read the data that was fetched by
+// ReadFrom().
 type bufferedReader struct {
 	// ready channel is closed when we're ready to read
 	ready chan struct{}
-	buf   *bytes.Buffer
+	buf   *bufio.Reader
 	pool  *bufferPool
 }
 
 var _ io.Reader = &bufferedReader{}
-
-var emptyBuffer = bytes.NewBuffer(nil)
+var _ io.ReaderFrom = &bufferedReader{}
 
 func newBufferedReader(pool *bufferPool) *bufferedReader {
 	return &bufferedReader{
@@ -35,57 +39,58 @@ func newBufferedReader(pool *bufferPool) *bufferedReader {
 // pool.
 func (b *bufferedReader) Read(buf []byte) (int, error) {
 	<-b.ready
+	if b.buf == nil {
+		return 0, io.EOF
+	}
 	n, err := b.buf.Read(buf)
 	// If we've read all the data,
-	if b.buf.Len() == 0 && b.buf != emptyBuffer {
+	if b.buf.Buffered() == 0 {
 		// return the buffer to the pool
 		b.pool.Put(b.buf)
 		// and replace our buffer with something that will always return EOF on
 		// future reads
-		b.buf = emptyBuffer
+		b.buf = nil
 	}
 	return n, err
 }
 
-func (b *bufferedReader) done() {
-	close(b.ready)
+func (b *bufferedReader) ReadFrom(r io.Reader) (int64, error) {
+	b.buf.Reset(r)
+	bytes, err := b.buf.Peek(b.buf.Size())
+	if err == io.EOF {
+		// ReadFrom does not return io.EOF
+		err = nil
+	}
+	return int64(len(bytes)), err
 }
 
-func (b *bufferedReader) downloadBody(resp *http.Response) error {
-	expectedBytes := resp.ContentLength
-
-	if expectedBytes > int64(b.buf.Cap()) {
-		return fmt.Errorf("Tried to download 0x%x bytes to a 0x%x-sized buffer", expectedBytes, b.buf.Cap())
-	}
-	n, err := b.buf.ReadFrom(resp.Body)
-	if err != nil && err != io.EOF {
-		return fmt.Errorf("error reading response for %s: %w", resp.Request.URL.String(), err)
-	}
-	if n != expectedBytes {
-		return fmt.Errorf("downloaded %d bytes instead of %d for %s", n, expectedBytes, resp.Request.URL.String())
-	}
-	return nil
+func (b *bufferedReader) Done() {
+	close(b.ready)
 }
 
 type bufferPool struct {
 	pool sync.Pool
 }
 
-func newBufferPool(capacity int64) *bufferPool {
+func newBufferPool(size int64) *bufferPool {
 	return &bufferPool{
 		pool: sync.Pool{
 			New: func() any {
-				return bytes.NewBuffer(make([]byte, 0, capacity))
+				return bufio.NewReaderSize(nil, int(size))
 			},
 		},
 	}
 }
 
-func (p *bufferPool) Get() *bytes.Buffer {
-	return p.pool.Get().(*bytes.Buffer)
+var emptyReader = strings.NewReader("")
+
+// Get returns a bufio.Reader with the correct size, with a blank underlying io.Reader.
+func (p *bufferPool) Get() *bufio.Reader {
+	br := p.pool.Get().(*bufio.Reader)
+	br.Reset(emptyReader)
+	return br
 }
 
-func (p *bufferPool) Put(buf *bytes.Buffer) {
-	buf.Reset()
+func (p *bufferPool) Put(buf *bufio.Reader) {
 	p.pool.Put(buf)
 }
--- pkg/download/common.go
+++ pkg/download/common.go
@@ -1,6 +1,7 @@
 package download
 
 import (
+	"fmt"
 	"regexp"
 
 	"github.com/dustin/go-humanize"
@@ -9,3 +10,14 @@ import (
 const defaultChunkSize = 125 * humanize.MiByte
 
 var contentRangeRegexp = regexp.MustCompile(`^bytes .*/([0-9]+)$`)
+
+type ErrContentLengthMismatch struct {
+	contentLength   int64
+	downloadedBytes int64
+}
+
+var _ error = ErrContentLengthMismatch{}
+
+func (err ErrContentLengthMismatch) Error() string {
+	return fmt.Sprintf("Downloaded %d bytes but Content-Length was %d", err.downloadedBytes, err.contentLength)
+}

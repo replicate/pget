a1dfdc8d8878a3fc61b311872191749e17d231e5
Dan Buch <dan@meatballhat.com>
2024-12-02 20:03:16 -0500

    Using `require` instead of `assert`
    and `*testing.T.Cleanup` instead of `defer` plus constifying `1024`

--- pkg/consumer/null_test.go
+++ pkg/consumer/null_test.go
@@ -4,20 +4,19 @@ import (
 	"bytes"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/replicate/pget/pkg/consumer"
 )
 
 func TestNullWriter_Consume(t *testing.T) {
-	buf := generateTestContent(1024)
+	r := require.New(t)
+	buf := generateTestContent(kB)
 	reader := bytes.NewReader(buf)
 
 	nullConsumer := consumer.NullWriter{}
-	err := nullConsumer.Consume(reader, "", 1024)
-	assert.NoError(t, err)
+	r.NoError(nullConsumer.Consume(reader, "", kB))
 
 	_, _ = reader.Seek(0, 0)
-	err = nullConsumer.Consume(reader, "", 100)
-	assert.Error(t, err)
+	r.Error(nullConsumer.Consume(reader, "", kB-100))
 }
--- pkg/consumer/write_file_test.go
+++ pkg/consumer/write_file_test.go
@@ -5,47 +5,47 @@ import (
 	"os"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/replicate/pget/pkg/consumer"
 )
 
 func TestFileWriter_Consume(t *testing.T) {
+	r := require.New(t)
 
-	buf := generateTestContent(1024)
+	buf := generateTestContent(kB)
 	reader := bytes.NewReader(buf)
 
 	writeFileConsumer := consumer.FileWriter{}
 	tmpFile, _ := os.CreateTemp("", "fileWriterTest-")
 
-	defer tmpFile.Close()
-	defer os.Remove(tmpFile.Name())
+	t.Cleanup(func() {
+		tmpFile.Close()
+		os.Remove(tmpFile.Name())
+	})
 
-	err := writeFileConsumer.Consume(reader, tmpFile.Name(), 1024)
-	assert.NoError(t, err)
+	r.NoError(writeFileConsumer.Consume(reader, tmpFile.Name(), kB))
 
 	// Check the file content is correct
 	fileContent, _ := os.ReadFile(tmpFile.Name())
-	assert.Equal(t, buf, fileContent)
+	r.Equal(buf, fileContent)
 
 	_, _ = reader.Seek(0, 0)
-	err = writeFileConsumer.Consume(reader, "", 100)
-	assert.Error(t, err)
+	r.Error(writeFileConsumer.Consume(reader, "", kB-100))
 
 	// test overwrite
 	// overwrite the file
 	f, err := os.OpenFile(tmpFile.Name(), os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
-	assert.NoError(t, err)
+	r.NoError(err)
 	_, _ = f.Write([]byte("different content"))
 	f.Close()
 
 	// consume the reader
 	_, _ = reader.Seek(0, 0)
 	writeFileConsumer.Overwrite = true
-	err = writeFileConsumer.Consume(reader, tmpFile.Name(), 1024)
-	assert.NoError(t, err)
+	r.NoError(writeFileConsumer.Consume(reader, tmpFile.Name(), kB))
 
 	// check the file content is correct
 	fileContent, _ = os.ReadFile(tmpFile.Name())
-	assert.Equal(t, buf, fileContent)
+	r.Equal(buf, fileContent)
 }

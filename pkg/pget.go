48e9645915d9012923bdee9749069f0892f0429c
Nikhil Sinha <nikhil@replicate.com>
2025-05-30 17:20:26 -0700

    Add throughput to metrics

--- pkg/pget.go
+++ pkg/pget.go
@@ -64,18 +64,22 @@ func (g *Getter) DownloadFile(ctx context.Context, url string, dest string) (int
 	// downloadElapsed := time.Since(downloadStartTime)
 	// writeStartTime := time.Now()
 
-	// Fire and forget metrics
-	go func() {
-		g.sendMetrics(url, fileSize)
-	}()
-
 	err = g.Consumer.Consume(buffer, dest, fileSize)
 	if err != nil {
+		// Fire and forget metrics
+		go func() {
+			g.sendMetrics(url, fileSize, 0, true)
+		}()
 		return fileSize, 0, fmt.Errorf("error writing file: %w", err)
 	}
 
 	// writeElapsed := time.Since(writeStartTime)
 	totalElapsed := time.Since(downloadStartTime)
+
+	// Fire and forget metrics
+	go func() {
+		g.sendMetrics(url, fileSize, (float64(fileSize) / totalElapsed.Seconds()), false)
+	}()
 
 	size := humanize.Bytes(uint64(fileSize))
 	// downloadThroughput := humanize.Bytes(uint64(float64(fileSize) / downloadElapsed.Seconds()))
@@ -144,17 +148,24 @@ func (g *Getter) downloadAndMeasure(ctx context.Context, url, dest string, total
 	return nil
 }
 
-func (g *Getter) sendMetrics(url string, size int64) {
+func (g *Getter) sendMetrics(url string, size int64, throughput float64, downloadFailed bool) {
 	logger := logging.GetLogger()
 	endpoint := viper.GetString(config.OptMetricsEndpoint)
 	if endpoint == "" {
 		return
 	}
 
+	data := map[string]any{"url": url, "size": size, "version": version.GetVersion()}
+	if downloadFailed {
+		data["error"] = true
+	} else {
+		data["throughput"] = throughput
+	}
+
 	payload := MetricsPayload{
 		Source: "pget",
 		Type:   "download",
-		Data:   map[string]any{"url": url, "size": size, "version": version.GetVersion()},
+		Data:   data,
 	}
 	body, err := json.Marshal(payload)
 	if err != nil {
